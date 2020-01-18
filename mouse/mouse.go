8da1a6ebad1b80ea39b48de99e25056a38c688d4
Helmut Kemper <helmut.kemper@gmail.com>
2020-01-18 20:44:26 -0300

    mouse channels

--- mouse/mouse.go
+++ mouse/mouse.go
@@ -1,10 +1,31 @@
 package mouse
 
-type Channel struct {
+// cursor name
+type Cursor struct {
 	Name string
 }
 
-type CoordinateChannel struct {
+type Coordinate struct {
 	X int
 	Y int
+}
+
+type Move Coordinate
+type Click Coordinate
+type DoubleClick Coordinate
+type Press Coordinate
+type Release Coordinate
+
+var BrowserMouseToPlatformMouseCoordinate chan Move
+var BrowserMouseClickToPlatformMouseClickEvent chan Click
+var BrowserMouseDoubleClickToPlatformMouseDoubleClickEvent chan DoubleClick
+var BrowserMouseDownToPlatformMouseDownEvent chan Press
+var BrowserMouseUpToPlatformMouseUpEvent chan Release
+
+func init() {
+	BrowserMouseToPlatformMouseCoordinate = make(chan Move)
+	BrowserMouseClickToPlatformMouseClickEvent = make(chan Click)
+	BrowserMouseDoubleClickToPlatformMouseDoubleClickEvent = make(chan DoubleClick)
+	BrowserMouseDownToPlatformMouseDownEvent = make(chan Press)
+	BrowserMouseUpToPlatformMouseUpEvent = make(chan Release)
 }
